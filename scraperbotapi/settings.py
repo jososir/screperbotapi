2f34c2613620196ccb6f7c28c07bb3c4d81c3bde
BASU SIR <me@basusir.com>
2023-08-10 06:49:25 +0530

    migraction add static file craction command run

--- scraperbotapi/settings.py
+++ scraperbotapi/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
